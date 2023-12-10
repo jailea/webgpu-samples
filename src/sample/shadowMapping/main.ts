c6530d4190d1ce149ee52e673b941c0411a4751a
Cryszzz <69176305+Cryszzz@users.noreply.github.com>
2023-12-10 16:20:48 -0500

    change code for skybox

--- src/sample/shadowMapping/main.ts
+++ src/sample/shadowMapping/main.ts
@@ -926,7 +926,7 @@ const init: SampleInit = async ({ canvas, pageState, gui, stats }) => {
       passEncoder.draw(6,(heightTexture.width-1)*(heightTexture.height-1));//(heightTexture.width-1)*(heightTexture.height-1)
       // passEncoder.setIndexBuffer(indexBuffer, 'uint16');
       // passEncoder.drawIndexed(indexCount);
-      renderSkybox(device, canvas, skyboxViewMatrix, projectionMatrix, skyboxPipeline, skyboxVerticesBuffer, skyboxUniformBuffer, skyboxUniformBindGroup,passEncoder,cameraViewProj);
+      renderSkybox(device, skyboxPipeline, skyboxVerticesBuffer, skyboxUniformBuffer, skyboxUniformBindGroup,passEncoder,cameraViewProj);
       passEncoder.end();
     }
     
